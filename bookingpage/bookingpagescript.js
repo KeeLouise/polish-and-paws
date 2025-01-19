dec4eb993744f25d5e9edcfb00c30ff8e289a650
KeeLouise <keelouise@yahoo.com>
2025-01-19 00:01:17 +0000

    Booking page added

---
+++ bookingpage/bookingpagescript.js
@@ -0,0 +1,26 @@
+//Nav Script
+const navLinks = document.querySelectorAll('nav a');
+
+function enlargeLink(event) {
+  event.target.style.transform = 'scale(1.2)';
+  event.target.style.color = '#89CFF0';
+}
+
+function resetLink(event) {
+  event.target.style.transform = 'scale(1)';
+  event.target.style.color = 'white';
+}
+
+navLinks.forEach(link => {
+  link.addEventListener('mouseover', enlargeLink);
+  link.addEventListener('mouseout', resetLink);
+});
+
+//Booking form script
+const form = document.getElementById('booking-form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    alert('Booking received! We will be in contact shortly!');
+});
