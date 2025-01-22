d379e8b6899ac83332f0d0cf599d44342096a07f
KeeLouise <keelouise@yahoo.com>
2025-01-22 00:29:41 +0000

    Update for slideshow
    Troubleshooting for slideshow - test

--- homepagescript.js
+++ homepagescript.js
@@ -82,9 +82,10 @@ function showSlides() {
   if (slideIndex > slides.length) {
     slideIndex = 1;
   }
-  slides[slideIndex 0].style.display = 'block';
-  dots[slideIndex 0].classList.add('active');
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].classList.add('active');
 }
+
 function startAutoPlay() {
   autoPlayInterval = setInterval(() => {
     showSlides();
@@ -118,4 +119,3 @@ function initSlider() {
 }
 
 document.addEventListener('DOMContentLoaded', initSlider);
-
